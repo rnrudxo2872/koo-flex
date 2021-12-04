99bedb9340a1525a2e9001c58ebff73b7fc63b80
unknown <rnrudxo2872@naver.com>
2021-12-04 15:51:06 +0900

    search ani

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import { Link, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
 
@@ -8,6 +9,7 @@ const Nav = styled.nav`
   width: 100%;
   position: fixed;
   top: 0;
+  justify-content: space-between;
 `;
 
 const Col = styled.div`
@@ -45,22 +47,40 @@ const Item = styled(motion.li)`
 `;
 
 const Pointer = styled(motion.div)`
-  width: 5px;
-  height: 5px;
+  width: 8px;
+  height: 8px;
   background-color: red;
   border-radius: 100%;
   position: absolute;
   left: 0;
   right: 0;
   margin: 0 auto;
-  bottom: -10px;
+  bottom: -12px;
+`;
+
+const SearchWrapper = styled(motion.div)`
+  margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SearchInput = styled(motion.input)`
+  transform-origin: center right;
+  margin-right: 5px;
+`;
+
+const SearchSvg = styled(motion.svg)`
+  height: 17px;
 `;
 
 function Header() {
+  const [onSearch, setOnSearch] = useState(false);
   const HomeMatch = useRouteMatch("/");
   const TvMatch = useRouteMatch("/tv");
+  const PointerId = "HeaderPointer";
 
-  console.log(HomeMatch, TvMatch);
+  const OnClickSearch = () => setOnSearch((prev) => !prev);
 
   return (
     <Nav>
@@ -76,25 +96,44 @@ function Header() {
         <List>
           <Item>
             <Link to="/">Home</Link>
-            {HomeMatch?.isExact && <Pointer />}
+            {HomeMatch?.isExact && <Pointer layoutId={PointerId} />}
           </Item>
           <Item>
             <Link to="/tv">TV Shows</Link>
-            {TvMatch && <Pointer />}
+            {TvMatch && <Pointer layoutId={PointerId} />}
           </Item>
           <Item>
             <Link to="/movies">Movies</Link>
-            {null && <Pointer />}
+            {null && <Pointer layoutId={PointerId} />}
           </Item>
           <Item>
             <Link to="/new-popular">New & Popular</Link>
-            {null && <Pointer />}
+            {null && <Pointer layoutId={PointerId} />}
           </Item>
           <Item>
             <Link to="/mylist">My List</Link>
-            {null && <Pointer />}
+            {null && <Pointer layoutId={PointerId} />}
           </Item>
         </List>
+      </Col>
+      <Col>
+        <SearchWrapper>
+          <SearchInput
+            initial={{ scaleX: 0 }}
+            animate={{ scaleX: onSearch ? 1 : 0 }}
+            placeholder="search content"
+          />
+          <SearchSvg
+            onClick={OnClickSearch}
+            animate={{ x: onSearch ? -240 : 0 }}
+            aria-hidden="true"
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+          </SearchSvg>
+        </SearchWrapper>
       </Col>
     </Nav>
   );
